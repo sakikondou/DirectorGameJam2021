02c49686a2a6251f3ffac558ea290641d3fb2983
M1namiK <min124k373@gmail.com>
2021-08-07 14:28:14 +0900

    弾のスクリプトです（プレイヤータグ追加）

---
+++ Assets/Minami/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float speed = 5.0f;
+
+    private Rigidbody2D rb;
+    private Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        rb.velocity = direction * speed;
+       
+    }
+}
