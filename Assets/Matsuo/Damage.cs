5251c7ca0065e432568e842ff13143d92a9738e8
Roku <mr558869193@gmail.com>
2021-08-07 13:59:54 +0900

    追加：ダメージスクリプト　バレットタグ

---
+++ Assets/Matsuo/Damage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    [SerializeField] int m_life = 1;
+    [SerializeField] GameObject m_explosionPrefab = null;
+
+  
+
+ 
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))  // 衝突相手が 弾 だったら
+        {
+            Destroy(collision.gameObject);  // 弾のオブジェクトを破棄する
+            m_life--;   // ライフを減らす
+            Debug.Log("ダメージ");
+
+            // ライフが 0 だったら
+            if (m_life < 1)
+            {
+                // 爆発エフェクトを生成する
+                if (m_explosionPrefab)
+                {
+                    Instantiate(m_explosionPrefab, this.transform.position, m_explosionPrefab.transform.rotation);
+                }
+                Destroy(this.gameObject);       // そして自分も破棄する
+               
+            }
+        }
+    }
+
+}
