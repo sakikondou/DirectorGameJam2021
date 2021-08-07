fd328577831e527a0ef65a30a34ff6e1ae327b9d
Roku <mr558869193@gmail.com>
2021-08-07 14:27:14 +0900

    追加：ダメージスクリプト

--- Assets/Matsuo/Damage.cs
+++ Assets/Matsuo/Damage.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class Damage : MonoBehaviour
 {
-    [SerializeField] int m_life = 1;
+    [SerializeField] int m_life = 10;
     [SerializeField] GameObject m_explosionPrefab = null;
 
-  
 
- 
+
+
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Bullet"))  // 衝突相手が 弾 だったら
         {
+            Debug.Log("ダメージ");
             Destroy(collision.gameObject);  // 弾のオブジェクトを破棄する
             m_life--;   // ライフを減らす
-            Debug.Log("ダメージ");
 
             // ライフが 0 だったら
             if (m_life < 1)
@@ -28,9 +28,9 @@ public class Damage : MonoBehaviour
                     Instantiate(m_explosionPrefab, this.transform.position, m_explosionPrefab.transform.rotation);
                 }
                 Destroy(this.gameObject);       // そして自分も破棄する
-               
+                Debug.Log("破壊");
+
             }
         }
     }
-
 }
