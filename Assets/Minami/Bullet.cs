27f8cb72f695c446fb95cd832cd288bb75ac8e6b
tetora ikarashi <teto670013@gmail.com>
2021-08-07 16:21:21 +0900

    Bombに弾が当たった時の処理、Bulletを飛ばす処理、Prefabの作成

---
+++ Assets/Minami/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Tooltip("弾の速度"), SerializeField] float m_speed = 5.0f;
+    private Rigidbody2D m_rb;
+    private Vector2 m_direction;
+
+    public void Init(Transform muzzle, Transform gun)
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+        m_direction = muzzle.position - gun.position;
+        m_rb.velocity = m_direction * m_speed;
+    }
+}
