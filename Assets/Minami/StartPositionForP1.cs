34716d2811d12d0b27333e1a5422999930db8ae8
M1namiK <min124k373@gmail.com>
2021-08-07 19:58:48 +0900

    プレイヤーの初期座標取得（２機分、乱数から）更新しました。

---
+++ Assets/Minami/StartPositionForP1.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class StartPositionForP1 : MonoBehaviour
+{
+    public GameObject m_Player1Prefab;
+    public GameObject m_Player2Prefab;
+
+    void Start()
+    {
+        //スクリーン端座標
+        var screen_top = 5.0f;
+        var screen_bottom = -5.0f;
+        var screen_left = -7.5f;
+
+        //プレイヤーサイズ
+        float player_width = GetComponent<Transform>().localScale.x;
+        float player_height = GetComponent<Transform>().localScale.y;
+
+        var playerPos_x = Random.Range(screen_left + player_width, -1.5f) ;
+        var playerPos_y = Random.Range(screen_bottom + player_height, screen_top - player_width); 
+
+
+        //１Ｐと２Ｐ生成
+        GameObject Player1 = Instantiate(m_Player1Prefab, new Vector2(playerPos_x , playerPos_y),Quaternion.identity);
+        GameObject Player2 = Instantiate(m_Player2Prefab, new Vector2(-playerPos_x , -playerPos_y),Quaternion.identity);
+
+    }
+
+}
---
+++ Assets/Minami/StartPositionForP2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPositionForP2 : MonoBehaviour
+{
+    public GameObject m_Player2Prefab;
+
+    void Start()
+    {
+        //１Ｐの座標を取得
+        Vector2 Player1_pos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        GameObject.FindGameObjectWithTag("Player").transform.position = new Vector2(Player1_pos.x,Player1_pos.y);
+
+
+        //１Ｐの座標と対になるように２Ｐの座標を取得
+        GameObject Player2 = Instantiate(m_Player2Prefab, new Vector2(-Player1_pos.x, -Player1_pos.y), Quaternion.identity);
+        //GameObject Player2 = Instantiate(m_Player2Prefab, new Vector2(10, 10), Quaternion.identity);
+
+    }
+
+}
